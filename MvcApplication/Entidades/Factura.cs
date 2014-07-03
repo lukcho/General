4da0b0351c2396aa060bc6d306dd63f42856d96f
Lukcho <luiscorrea1988@hotmail.com>
2014-07-02 21:41:00 -0500

    Initial commit

---
+++ MvcApplication/Entidades/Factura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication.Entidades
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        
+        public string Cedula { get; set; }
+
+        [Required]
+        public string ClienteNombre { get; set; }
+
+        [Required]
+        public ICollection<Detalle> Detalle { get; set; }
+
+        public Factura()
+        {
+            this.Detalle = new HashSet<Detalle>();
+        }
+    }
+}
